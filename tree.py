f8a174a7ad6272bfa9bdda47dbd3a4abaf8431fc
runoguler <onurguler93@gmail.com>
2018-10-22 19:50:01 +0900

    Incompleted Dynamic Tree CNN Implementation

--- tree.py
+++ tree.py
@@ -145,29 +145,28 @@ def test_tree(models, test_loader, device, use_cuda):
 
 def train_dynamic_tree(models, leaf_node_labels, train_loader, device, epoch, args, use_cuda):
     leaf_node_index = []
-    leaf_node_paths = []
-    losses = []
+    leaf_node_paths = []    # NOT INCLUDING models[0]
+
     for i in range(len(models)):
-        if isinstance(models[i], MobileTreeLeafNet):
-            leaf_node_index.append(i)
         if not models[i] is None:
             models[i].train()
-        else:
-            losses.append(None)
+            if isinstance(models[i], MobileTreeLeafNet):
+                leaf_node_index.append(i)
 
+    losses = []
     optims = []
     for i in leaf_node_index:
-        temp = []
+        path = []
         while i > 0:
-            temp.append(i)
+            path = [i] + path
             i = (i+1)//2 - 1
-        leaf_node_paths.append(temp)
-        path = list(models[0].parameters())
-        for i in reversed(temp):
-            path += list(models[i].parameters())
-        losses.append(torch.nn.CrossEntropyLoss())
-        losses[i].to(device)
-        optims.append(torch.optim.Adam(path, lr=args.lr, betas=(0.5, 0.999)))
+        model_path = list(models[0].parameters())
+        for i in path:
+            model_path += list(models[i].parameters())
+
+        leaf_node_paths.append(path)
+        losses.append(torch.nn.CrossEntropyLoss().to(device))
+        optims.append(torch.optim.Adam(model_path, lr=args.lr, betas=(0.5, 0.999)))
 
 
     for batch_idx, (data, labels) in enumerate(train_loader):
@@ -177,115 +176,103 @@ def train_dynamic_tree(models, leaf_node_labels, train_loader, device, epoch, ar
             optims[i].zero_grad()
 
         layer = models[0](data)
-        for i in range(len(leaf_node_paths)):
+        for i in range(len(leaf_node_paths)):   # for every branch(path) going to a leaf node
             for j in leaf_node_paths[i]:
                 k = leaf_node_paths[i][j]
                 if j+1 == len(leaf_node_paths[i]):
                     result, _= models[k](layer)
-                    ## TODO Change Labels for each Leaf Node, calculate the loss and step the optimizer
+
+                    lbls = labels.clone()
+                    for l in range(len(lbls)):
+                        if lbls[l] in leaf_node_labels[i]:
+                            lbls[l] = leaf_node_labels[i].index(lbls[l])
+                        else:
+                            lbls[l] = len(leaf_node_labels[i])
+
+                    l = losses[i](result, lbls)
+                    l.backward(retain_graph=True)
+                    optims[i].step()
                 else:
                     layer = models[k](layer)
 
-
-        b1_labels = labels.clone()
-        b2_labels = labels.clone() - 5
-
-        b1_labels[b1_labels > 4] = 5
-        b2_labels[b2_labels < 0] = 5
-
-        # b1_labels = torch.zeros((labels.size(0), 5), device=device)
-        # b2_labels = torch.zeros((labels.size(0), 5), device=device)
-        #
-        # for i in range(labels.size(0)):
-        #     if labels[i].item() < 5:
-        #         b1_labels[i][labels[i].item()] = 1
-        #     else:
-        #         b2_labels[i][labels[i].item()-5] = 1
-
-        b1_loss = loss_b1(out_b1, b1_labels)
-        b1_loss.backward(retain_graph=True)  ## retain_graph=True
-        optim_b1.step()
-
-        b2_loss = loss_b2(out_b2, b2_labels)
-        b2_loss.backward()
-        optim_b2.step()
-
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tB1 Loss: {:.6f}\tB2 Loss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), b1_loss.item(), b2_loss.item()))
+            p_str = 'Train Epoch: {} [{}/{} ({:.0f}%)]'
+            for loss in losses:
+                p_str += '\tLoss: {:.6f}'.format(loss.item())
 
-        # if batch_idx % args.log_interval == 0:
-        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        #         epoch, batch_idx * len(data), len(train_loader.dataset),
-        #                100. * batch_idx / len(train_loader), l.item(), ))
+            print(p_str.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader)))
 
 
 def test_dynamic_tree(models, leaf_node_labels, test_loader, device, use_cuda):
-    models[0].eval()
-    models[1].eval()
-    models[2].eval()
+    leaf_node_index = []
+    leaf_node_paths = []  # NOT INCLUDING models[0]
 
-    # correct_b1 = 0
-    # correct_b2 = 0
-    corrects = 0
-    no_class = 0
-    both_class = 0
-    false_in_class = 0
-    correct_from_both = 0
-    max_correct_from_both = 0
+    for i in range(len(models)):
+        if not models[i] is None:
+            models[i].eval()
+            if isinstance(models[i], MobileTreeLeafNet):
+                leaf_node_index.append(i)
+
+    for i in leaf_node_index:
+        path = []
+        while i > 0:
+            path = [i] + path
+            i = (i+1)//2 - 1
+        leaf_node_paths.append(path)
+
+    definite_correct = 0
+    indefinite_correct = 0
+    wrong = 0
 
     for data, label in test_loader:
         data, labels = data.to(device), label.to(device)
 
+        pred = []
         layer = models[0](data)
-        out_b1, _ = models[1](layer)
-        out_b2, _ = models[2](layer)
+        for i in range(len(leaf_node_paths)):  # for every branch(path) going to a leaf node
+            for j in leaf_node_paths[i]:
+                k = leaf_node_paths[i][j]
+                if j + 1 == len(leaf_node_paths[i]):
+                    result, _ = models[k](layer)
 
-        pred_b1 = out_b1.max(1, keepdim=True)[1]
-        pred_b2 = out_b2.max(1, keepdim=True)[1]
+                    lbls = labels.clone()
+                    for l in range(len(lbls)):
+                        if lbls[l] in leaf_node_labels[i]:
+                            lbls[l] = leaf_node_labels[i].index(lbls[l])
+                        else:
+                            lbls[l] = len(leaf_node_labels[i])
+
+                    pred.append(result.max(1, keepdim=True)[1])
+                else:
+                    layer = models[k](layer)
 
         for i in range(labels.size(0)):
-            if pred_b1[i] == 5:
-                if pred_b2[i] == 5:
-                    no_class += 1
+            lbl = labels[i].item()
+            ln_index = -1
+            for j in range(len(leaf_node_labels)):
+                if lbl in leaf_node_labels[j]:
+                    k = leaf_node_labels[j].index(lbl)
+                    ln_index = (j, k)
+                    break
+            if pred[ln_index[0]][i] == ln_index[1]:
+                definite = True
+                for j in range(len(leaf_node_index)):
+                    if j != ln_index[0]:
+                        if pred[j][i] != len(leaf_node_labels[j]):
+                            definite = False
+                if definite:
+                    definite_correct += 1
                 else:
-                    if labels[i].item() == (pred_b2[i].item() + 5):
-                        corrects += 1
-                    else:
-                        false_in_class += 1
+                    indefinite_correct += 1
             else:
-                if pred_b2[i] == 5:
-                    if labels[i].item() == pred_b1[i].item():
-                        corrects += 1
-                    else:
-                        false_in_class += 1
-                else:
-                    both_class += 1
-                    if (labels[i].item() == (pred_b2[i].item() + 5)) or (labels[i].item() == pred_b1[i].item()):
-                        correct_from_both += 1
-                        if (out_b1[i][pred_b1[i].item()].item() > out_b2[i][pred_b2[i].item()].item()) and (
-                                labels[i].item() == pred_b1[i].item()):
-                            max_correct_from_both += 1
-                        elif (out_b2[i][pred_b2[i].item()].item() > out_b1[i][pred_b1[i].item()].item()) and (
-                                labels[i].item() == (pred_b2[i].item() + 5)):
-                            max_correct_from_both += 1
+                wrong += 1
 
-        # out = torch.cat((out_b1, out_b2), dim=1)
-
-        # pred = out.max(1, keepdim=True)[1]
-        # corrects += pred.eq(labels.view_as(pred)).sum().item()
-
-    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n\t  F_in: {} None: {} Both: ({}/{}/{})\n'.format(
-        corrects, len(test_loader.dataset),
-        100. * corrects / len(test_loader.dataset),
-        false_in_class, no_class, both_class, correct_from_both, max_correct_from_both
+    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n\tDefinite Corrects: {}\n'.format(
+        (definite_correct + indefinite_correct), len(test_loader.dataset),
+        100. * (definite_correct + indefinite_correct) / len(test_loader.dataset), definite_correct
     ))
-
-    # print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-    #     corrects, len(test_loader.dataset),
-    #     100. * corrects / len(test_loader.dataset)
-    # ))
 
 
 
@@ -443,6 +430,8 @@ def main():
                                                **cuda_args)
     test_loader = torch.utils.data.DataLoader(cifar_testing_data, batch_size=args.test_batch_size, shuffle=True,
                                               **cuda_args)
+
+    args.mobile_tree_net = True
 
     if args.simple_net:
         model = SimpleNet().to(device)
